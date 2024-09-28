99e030394448642634c47f3d840c857f20c6259a
Mohamed El tamawey <tammwy22@gmail.com>
2024-09-28 08:55:50 +0300

    fix

--- lua/pairwriter.lua
+++ lua/pairwriter.lua
@@ -2,8 +2,6 @@
 
 local M = {}
 
-local pairwriter_dir
-
 local function copy_function_pointers(src, dest)
 	for key, value in pairs(src) do
 		if type(value) == "function" then
@@ -13,26 +11,32 @@ local function copy_function_pointers(src, dest)
 end
 M.setup = function(t)
 	t = t or {}
-  local old_pacakge_cpath = package.cpath
+	t.username = t.username or "SERVER"
+	local old_pacakge_cpath = package.cpath
+
+	local pairwriter_dir
 	if vim.fn.has("unix") == 1 then
 		-- unix use
-		local pairwriter_dir_path = t.dir_path or "~/.local/share/nvim/pairwriter"
-		pairwriter_dir = vim.fn.expand(pairwriter_dir_path)
+		t.dir_path = t.dir_path or "~/.local/share/nvim/pairwriter"
+		pairwriter_dir = vim.fn.expand(t.dir_path)
 		package.cpath = pairwriter_dir .. "/?.so" .. ";" .. package.cpath
-    -- set he log file path
+		-- set the log file path
 		vim.cmd("let $LOGFILE='" .. pairwriter_dir .. "/log.log'")
 	else
 		-- windows
-		local pairwriter_dir_path = t.dir_path or "~\\AppData\\.local\\share\\nvim-data\\pairwriter" -- i don't know windows correct path
-		pairwriter_dir = vim.fn.expand(pairwriter_dir_path)
-		package.cpath = pairwriter_dir .. "/?.dll;" .. package.cpath
-    -- set he log file path
+		t.dir_path = t.dir_path or "~\\AppData\\.local\\share\\nvim-data\\pairwriter"
+		pairwriter_dir = vim.fn.expand(t.dir_path)
+		package.cpath = pairwriter_dir .. "\\?.dll;" .. package.cpath
+		-- set the log file path
 		vim.cmd("let $LOGFILE='" .. pairwriter_dir .. "\\log.log'")
 	end
-  t.username = t.username or "SERVER"
-	vim.cmd("let $SERVER_USERNAME='" .. t.username .."'")
+
+	vim.cmd("let $SERVER_USERNAME='" .. t.username .. "'")
+
+	M = {}
+
 	copy_function_pointers(require("pairwriter_helper"), M)
-  package.cpath = old_pacakge_cpath -- reset the package path 
+	package.cpath = old_pacakge_cpath -- reset the package path
 end
 
 return M
